4db42c8ba05d6e4a7f806095f085981523e2fc77
FlorR566 <florodriguez566@gmail.com>
2026-09-12 11:28:30 -0300

    refactor(vite): replace __dirname with import.meta.dirname for native ES module support

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,12 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				//d:aplicaion/dist/
-				index: resolve(__dirname, "index.html"),
-				login: resolve(__dirname, "src/pages/auth/login/login.html"),
-				registro: resolve(__dirname, "src/pages/auth/registro/registro.html"),
-				adminHome: resolve(__dirname, "src/pages/admin/home/home.html"),
-				clientHome: resolve(__dirname, "src/pages/client/home/home.html"),
-				//clientCart: resolve(__dirname, "scr/pages/client/cart.html"),
+				index: resolve(import.meta.dirname, "index.html"),
+				authLogin: resolve(import.meta.dirname, "src/pages/auth/login/login.html"),
+				authRegistro: resolve(import.meta.dirname, "src/pages/auth/registro/registro.html"),
+				adminHome: resolve(import.meta.dirname, "src/pages/admin/home/home.html"),
+				clientHome: resolve(import.meta.dirname, "src/pages/client/home/home.html"),
+				//clientCart: resolve(import.meta.dirname, "src/pages/client/cart/cart.html"),
 			},
 		},
 	},
