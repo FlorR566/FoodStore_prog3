e309445172c4146645f2c61995dcc92641b23cdb
FlorR566 <florodriguez566@gmail.com>
2026-09-14 12:31:46 -0300

    feat: add ICartItem interface

--- src/types/product.ts
+++ src/types/product.ts
@@ -12,3 +12,9 @@ export interface IProduct {
 	disponible: boolean;
 	categorias: ICategory[];
 }
+
+export interface ICartItem {
+	producto: IProduct; // Referencia al producto completo
+	cantidad: number; // Unidades seleccionadas por el cliente
+	subtotal: number; // Precio acumulado (producto.precio * cantidad)
+}
